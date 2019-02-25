24767cb286135cb06b896650d2a5764cabfc2d57
Theo Rivera <theorivera29.tmk@gmail.com>
2019-02-26 00:09:20 +0800

    Added register account funtion

--- Materials Engineer/createaccount.php
+++ Materials Engineer/createaccount.php
@@ -12,34 +12,33 @@
             <div class="jumbotron my-auto">
                 <h2>New Golden City Builders</h2>
                 <h2>Create Account</h2>
-                <form>
+                <form action="server.php" method="POST">
                     <div class="form-group">
-                        <input class="form-control form-control-lg" type="firstname" id="inputFirstname" placeholder="firstname">
+                        <input class="form-control form-control-lg" type="text" name="firstname" id="inputFirstname" placeholder="firstname">
                     </div>
                     <div class="form-group">
-                        <input class="form-control form-control-lg" type="lastname" id="inputCreateLastname" placeholder="lastname">
+                        <input class="form-control form-control-lg" type="text" name="lastname" id="inputCreateLastname" placeholder="lastname">
                     </div>
                     <div class="form-group">
-                        <input class="form-control form-control-lg" type="username" id="inputCreateUsername" placeholder="username">
+                        <input class="form-control form-control-lg" type="text" name="username" id="inputCreateUsername" placeholder="username">
                     </div>
                     <div class="form-group">
-                        <input class="form-control form-control-lg" type="email" id="inputCreateEmail" placeholder="email">
+                        <input class="form-control form-control-lg" type="text" name="email" id="inputCreateEmail" placeholder="email">
                     </div>
                     <div class="form-group">
-                        <input class="form-control form-control-lg" type="password" id="inputCreatePassword" placeholder="password">
+                        <input class="form-control form-control-lg" type="password" name="password" id="inputCreatePassword" placeholder="password">
                     </div>
-                    <!--NOT YET WORKING-->
                     <div class="custom-control custom-radio custom-control-inline">
-                        <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+                        <input type="radio" id="account_type" name="account_type" class="custom-control-input" value="Materials Engineer">
                         <label class="custom-cotrol-label" for="customRadioInline1">Materials Engineer</label>
                     </div>
                     <div class="custom-control custom-radio custom-control-inline">
-                        <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+                        <input type="radio" id="account_type" name="account_type" class="custom-control-input" value="View Only">
                         <label class="custom-cotrol-label" for="customRadioInline1">View Only</label>
                     </div>
 
                     <div>
-                        <button type="submit" class="btn btn-primary btn-block">Create</button>
+                        <button type="submit" class="btn btn-primary btn-block" name="create_account">Create</button>
                         <button type="submit" class="btn btn-primary btn-block">Cancel</button>
                     </div>                   
                 </form>

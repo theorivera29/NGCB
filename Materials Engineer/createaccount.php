02447341606d14eed835d3a80c189c2a7621f305
Jam <jamspica25@gmail.com>
2019-03-02 01:42:30 +0900

    Updated create account interface

--- Materials Engineer/createaccount.php
+++ Materials Engineer/createaccount.php
@@ -1,48 +1,102 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <link rel="stylesheet" text="type/css" href="../Bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" text="type/css" href="../style.css">
-    </head>
 
-    <body>
-        <!--Create account container-->
-        <div class="loginContainer container h-100 d-flex justify-content-center">
-            <div class="jumbotron my-auto">
-                <h2>New Golden City Builders</h2>
-                <h2>Create Account</h2>
-                <form action="server.php" method="POST">
-                    <div class="form-group">
-                        <input class="form-control form-control-lg" type="text" name="firstname" id="inputFirstname" placeholder="firstname">
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control form-control-lg" type="text" name="lastname" id="inputCreateLastname" placeholder="lastname">
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control form-control-lg" type="text" name="username" id="inputCreateUsername" placeholder="username">
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control form-control-lg" type="text" name="email" id="inputCreateEmail" placeholder="email">
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control form-control-lg" type="password" name="password" id="inputCreatePassword" placeholder="password">
-                    </div>
-                    <div class="custom-control custom-radio custom-control-inline">
-                        <input type="radio" id="account_type" name="account_type" class="custom-control-input" value="Materials Engineer">
-                        <label class="custom-cotrol-label" for="customRadioInline1">Materials Engineer</label>
-                    </div>
-                    <div class="custom-control custom-radio custom-control-inline">
-                        <input type="radio" id="account_type" name="account_type" class="custom-control-input" value="View Only">
-                        <label class="custom-cotrol-label" for="customRadioInline1">View Only</label>
-                    </div>
+<head>
+    <link rel="stylesheet" text="type/css" href="../materialize/css/materialize.css">
+    <link rel="stylesheet" text="type/css" href="../materialize/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" text="type/css" href="../style.css">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/css/materialize.css" rel="stylesheet">
 
-                    <div>
-                        <button type="submit" class="btn btn-primary btn-block" name="create_account">Create</button>
-                        <button type="submit" class="btn btn-primary btn-block">Cancel</button>
-                    </div>                   
-                </form>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m6 offset-m3">
+                <div class="card white darken-1">
+                    <div class="card-content black-text">
+                        <h2 class="card-title center-align">New Golden City Builders</h2>
+                        <h2 class="card-title center-align">Create Account</h2>
+                        <div class="row">
+                            <div class="input-field col s12 m10 offset-m1">
+                                <input id="firstname" name="first name" type="text" class="validate">
+                                <label for="firstname">First Name</label>
+                            </div>
+
+                            <div class="input-field col s12 m8 offset-m1">
+                                <input id="lastname" name="lastname" type="text" class="validate">
+                                <label for="lastname">Last Name</label>
+                            </div>
+
+                            <div class="input-field col s12 m10 offset-m1">
+                                <input id="username" name="username" type="text" class="validate">
+                                <label for="username">Username</label>
+                            </div>
+
+                            <div class="input-field col s12 m10 offset-m1">
+                                <input id="email" name="email" type="text" class="validate">
+                                <label for="email">Email</label>
+                            </div>
+
+                            <div class="input-field col s12 m10 offset-m1">
+                                <input id="password" name="password" type="text" class="validate">
+                                <label for="password">Password</label>
+                            </div>
+                            
+                           <!--ETO VINCENT-->
+                           <div class="col s12 m10 offset-m1">
+                                <form action="#">
+                                    <p>
+                                        <span>Account Type</span>
+                                        <label>
+                                            <input class="with-gap" name="group1" type="radio" checked />
+                                            <span>Materials Engineer</span>
+                                            <input class="with-gap" name="group1" type="radio" checked />
+                                            <span>View Only</span>
+                                        </label>
+                                    </p>
+                                </form>
+                            </div>
+
+
+                            <!--HANGGANG HERE-->
+                            
+                            <div class="row center">
+                                <a class="btn waves-effect waves-light" type="submit" name="action" href="createaccount.html">Create
+                                    An Account</a>
+                                    <a class="waves-effect waves-light btn" type="submit" name="action" href="loginpage.html">Cancel</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </body>
+    </div>
+    <div id="confirmationModal" class="modal">
+        <div class="modal-content">
+            <p>Your password reset has been sent. Kindly wait for the confirmation of your request. Please check your
+                email for updates</p>>
+        </div>
+        <div class="modal-footer">
+            <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">OK</a>
+        </div>
+    </div>
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/js/materialize.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('.modal-trigger').leanModal();
+        });
+    </script>
+</body>
+
 </html>

681616257f69cda7704a0c7d8bc0a0ddb68a6d2c
Caryl Marie <2161397@slu.edu.ph>
2019-03-16 21:44:42 +0800

    Update hauled items.php
    added the script for the side nav

--- Materials Engineer/hauled items.php
+++ Materials Engineer/hauled items.php
@@ -139,10 +139,21 @@
         </div>
     </div>
 
-    <script src="js/materialize.min.js"></script>
+     <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/js/materialize.js"></script>
     <script>
+        // SIDEBAR
         $(document).ready(function() {
-            $('.modal-trigger').leanModal();
+            $('.button-collapse').sideNav({
+                menuWidth: 300, // Default is 300
+                edge: 'left', // Choose the horizontal origin
+                closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+                draggable: true // Choose whether you can drag to open on touch screens
+            });
+            // START OPEN
+            $('.button-collapse').sideNav('show');
+
         });
 
     </script>

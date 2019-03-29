86adb310b7cc0566456bbdf9b57068bb63ee0b74
Jam <jamspica25@gmail.com>
2019-03-29 23:38:41 +0900

    Added a new page
    for the account of enginner

---
+++ Materials Engineer/account.php
@@ -0,0 +1,165 @@
+<?php
+    include "db_connection.php";
+    session_start();
+
+    if(!isset($_SESSION['loggedin'])) {
+      header('Location: http://127.0.0.1/NGCB/Materials%20Engineer/loginpage.php');
+    }
+  ?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<title>NGCB</title>
+    <link rel="icon" type="image/png" href="../Images/NGCB_logo.png">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/css/materialize.css" rel="stylesheet">
+    <link rel="stylesheet" text="type/css" href="../style.css">
+</head>
+
+<body>
+    <nav>
+        <div class="nav-wrapper">
+            <a href="#" data-activates="mobile-demo" class="button-collapse show-on-large"><i class="material-icons">menu</i></a>
+            <span id="NGCB">NEW GOLDEN CITY BUILDERS AND DEVELOPMENT CORPORATION</span>
+            
+            <ul class="side-nav" id="mobile-demo">
+                <li class="collection-item avatar">
+                    <?php 
+                        if(isset($_SESSION['username'])) {
+                        $username = $_SESSION['username'];
+                        $sql = "SELECT * FROM accounts WHERE accounts_username = '$username'";
+                        $result = mysqli_query($conn, $sql);
+                        $row = mysqli_fetch_row($result);
+                    ?>
+                    <span class="title">
+                        <?php echo $row[1]." ".$row[2]; ?>
+                    </span>
+                    <span class="title">
+                        <?php echo $row[5]; }?>
+                    </span>
+                </li>
+
+                <li>
+                    <div class="divider"></div>
+                </li>
+
+                <li>
+                    <a href="dashboard.php">Dashboard</a>
+                </li>
+
+                <li>
+                    <div class="divider"></div>
+                </li>
+
+                <ul class="collapsible">
+                    <li>
+                        <a class="collapsible-header waves-effect waves-blue">Site<i class="material-icons right">keyboard_arrow_down</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a class="waves-effect waves-blue" href="projects.php">Projects</a></li>
+                                <li><a class="waves-effect waves-blue" href="sitematerials.php">Site Materials</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+
+                <li>
+                    <div class="divider"></div>
+                </li>
+
+                <ul class="collapsible">
+                    <li>
+                        <a class="collapsible-header waves-effect waves-blue">Hauling<i class="material-icons right">keyboard_arrow_down</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li>
+                                    <a class="waves-effect waves-blue" href="hauling.php">Fill out Hauling Form</a>
+                                </li>
+                                <li>
+                                    <a class="waves-effect waves-blue" href="hauled%20items.php">View Hauled Materials</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+
+                <li>
+                    <div class="divider"></div>
+                </li>
+
+                <li>
+                    <a class="waves-effect waves-blue" href="report.php">Report</a>
+                </li>
+
+                <li>
+                    <div class="divider"></div>
+                </li>
+                
+                <li>
+                    <a href="logout.php">Account</a>
+                </li>
+
+                <li>
+                    <div class="divider"></div>
+                </li>
+
+                <li>
+                    <a href="logout.php">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <!--Calendar and To do Row-->
+    <div class="row">
+        <div class="container">
+            <span>ACCOUNT SOMETHING</span>
+        </div>
+    </div>
+
+
+
+
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/js/materialize.js"></script>
+    <script>
+        // SIDEBAR
+        $(document).ready(function() {
+            $('.button-collapse').sideNav({
+                menuWidth: 300, // Default is 300
+                edge: 'left', // Choose the horizontal origin
+                closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+                draggable: true // Choose whether you can drag to open on touch screens
+            });
+            // START OPEN
+            $('.button-collapse').sideNav('show');
+
+
+            //DATEPICKER
+            $('.datepicker').pickadate({
+                selectMonths: true, // Creates a dropdown to control month
+                selectYears: 15, // Creates a dropdown of 15 years to control year,
+                closeOnSelect: false // Close upon selecting a date,
+            });
+
+        });
+
+        const btn = document.querySelector('#li-generate');
+        const inpt = document.querySelector('#inputValue');
+        const ul = document.querySelector('.ulList');
+
+        //const deleteLi = document.querySelector('.remove');
+
+        btn.addEventListener('click', liGenerate);
+        document.addEventListener('click', liDelete);
+
+        
+    </script>
+
+</body>
+
+</html>

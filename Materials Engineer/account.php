bac7c4b60f2c3c88c2e3524b71ba42ae80716005
jamspica <jamspica25@gmail.com>
2019-03-30 16:39:50 +0800

    Updated account.php

--- Materials Engineer/account.php
+++ Materials Engineer/account.php
@@ -120,7 +120,36 @@
     <!--Calendar and To do Row-->
     <div class="row">
         <div class="container">
-            <span>ACCOUNT SOMETHING</span>
+        <div class="co-login">
+                    <div class="f-login username input-field col s6 m8 offset-m1 ">
+                        <input id="login-username" name="username" type="text">
+                        <label class="active" for="username">Username</label>
+                    </div>
+                    <div class="f-login username input-field col s6 m8 offset-m1 ">
+                        <input id="login-username" name="username" type="text">
+                        <label class="active" for="username">First Name</label>
+                    </div>
+                    <div class="f-login username input-field col s6 m8 offset-m1 ">
+                        <input id="login-username" name="username" type="text">
+                        <label class="active" for="username">Last Name</label>
+                    </div>
+                    <div class="f-login username input-field col s6 m8 offset-m1 ">
+                        <input id="login-username" name="username" type="text">
+                        <label class="active" for="username">E-mail</label>
+                    </div>
+                    <div class="f-login password input-field col s6 m8 offset-m1">
+                        <input id="login-password" name="password" type="password">
+                        <label class="active" for="password">Password</label>
+                    </div>
+                    <div class="row">
+                        <div class="col s12 m24">
+                            <button class="btn waves-effect waves-light login-btn" type="submit"
+                                name="login">Save</button>
+                        </div>
+                        
+                    </div>
+                 
+                </div>
         </div>
     </div>
 

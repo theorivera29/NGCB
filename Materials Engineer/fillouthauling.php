36aad8aa07bcccae1832adfd524561384f806ad2
Caryl Marie <2161397@slu.edu.ph>
2019-04-07 13:00:29 +0800

    Update fillouthauling.php
    deleted my last update in unit hihi sorry

--- Materials Engineer/fillouthauling.php
+++ Materials Engineer/fillouthauling.php
@@ -165,21 +165,7 @@
                                 <tbody>
                                     <tr>
                                         <td><input type="text" name="quantity" id="quantity"></td>
-                                        <td><select id="unit" class="browser-default" name="unit">
-                                                <option value="" disabled selected>Choose your option</option>
-                                                <?php
-                                    $sql = "SELECT DISTINCT mat_unit FROM materials;";
-                                    $result = mysqli_query($conn, $sql);
-                                    while($row = mysqli_fetch_row($result)) {                         
-
-                                ?>
-                            <option value="<?php echo $row[0]; ?>">
-                                <?php echo $row[0]; ?>
-                            </option>
-                            <?php 
-                                    }
-                                ?>
-                                            </select></td>
+                                        <td><input id="unit" readonly type="text" class="validate" name="unit"></td>
                                         <td>
                                             <select id="materials" class="browser-default" name="articles">
                                                 <option value="" disabled selected>Choose your option</option>

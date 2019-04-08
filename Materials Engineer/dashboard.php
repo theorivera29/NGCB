fef713f6289488f3610e856589ef697fc1e46e87
jamspicaa <Jamspica25@gmail.com>
2019-04-09 04:00:23 +0800

    Updated the side navigation
    Not yet done kaya wag muna icopy sa ibang pages

--- Materials Engineer/dashboard.php
+++ Materials Engineer/dashboard.php
@@ -17,14 +17,14 @@
     <link rel="icon" type="image/png" href="../Images/NGCB_logo.png">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/css/materialize.min.css" rel="stylesheet">
-   
+
     <link rel="stylesheet" text="type/css" href="../style.css">
 </head>
 
 <body>
     <nav>
         <div class="nav-wrapper">
-            <a href="#" data-activates="mobile-demo" class="button-collapse show-on-large"><i
+            <a href="#" data-activates="navigation" class="button-collapse show-on-large menu-icon"><i
                     class="material-icons menuIcon">menu</i></a>
             <span id="NGCB">NEW GOLDEN CITY BUILDERS AND DEVELOPMENT CORPORATION</span>
             <?php 
@@ -35,48 +35,43 @@
                             $row = mysqli_fetch_row($result);
                         ?>
             <span id="acName">
-                
-                <ul >
-                <?php echo $row[1]." ".$row[2]; ?>
+
+                <ul>
+                    <?php echo $row[1]." ".$row[2]; ?>
                     <li class="down-arrow">
-                    
+
                         <a class="dropdown-button" href="#!" data-activates="dropdown" data-beloworigin="true"><i
                                 class="material-icons right">keyboard_arrow_down</i></a>
                     </li>
-                    
+
                 </ul>
                 <ul id="dropdown" class="dropdown-content collection">
                     <li><a class="waves-effect waves-blue" href="account.php">Account</a></li>
                     <li><a class="waves-effect waves-blue" href="../logout.php">Logout</a></li>
-                    
+
                 </ul>
             </span>
-            <ul class="side-nav" id="mobile-demo">
-
-                <li class="collection-item avatar">
+            <ul class="side-nav" id="navigation">
+                <li class="icon-container">
                     <ul>
                         <li class="acType">
-                            <img src="../Images/NGCB_logo.png">
-                            <?php echo $row[5]; }?>
+                            <img src="../Images/NGCB_logo.png" class="sidenav-logo">
                         </li>
                     </ul>
                 </li>
+                <h3 id="account-type"><?php echo $row[5]; }?></h3>
 
                 <li>
-                    <div class="divider"></div>
+                    <i class="material-icons left">dashboard</i><a class="waves-effect waves-blue"
+                        href="dashboard.php">Dashboard</a>
                 </li>
 
-                <li>
-                    <a href="dashboard.php">Dashboard</a>
-                </li>
-
-                <li>
-                    <div class="divider"></div>
-                </li>
 
                 <ul class="collapsible">
                     <li>
-                        <a class="collapsible-header waves-effect waves-blue">Site<i class="material-icons right">keyboard_arrow_down</i></a>
+                        <i class="material-icons left">place</i><a
+                            class="collapsible-header waves-effect waves-blue">Site<i
+                                class="material-icons right">keyboard_arrow_down</i></a>
                         <div class="collapsible-body">
                             <ul>
                                 <li><a class="waves-effect waves-blue" href="projects.php">Projects</a></li>
@@ -85,14 +80,14 @@
                         </div>
                     </li>
                 </ul>
+                
 
-                <li>
-                    <div class="divider"></div>
-                </li>
-
+                
                 <ul class="collapsible">
                     <li>
-                        <a class="collapsible-header waves-effect waves-blue">Hauling<i class="material-icons right">keyboard_arrow_down</i></a>
+                        <i class="material-icons left">local_shipping</i><a
+                            class="collapsible-header waves-effect waves-blue">Hauling<i
+                                class="material-icons right">keyboard_arrow_down</i></a>
                         <div class="collapsible-body">
                             <ul>
                                 <li>
@@ -107,12 +102,11 @@
                     </li>
                 </ul>
 
-                <li>
-                    <div class="divider"></div>
-                </li>
+
 
                 <li>
-                    <a class="waves-effect waves-blue" href="report.php">Report</a>
+                    <i class="material-icons left">receipt</i><a class="waves-effect waves-blue"
+                        href="report.php">Report</a>
                 </li>
             </ul>
         </div>
@@ -130,17 +124,19 @@
                         <label>To-do Date:</label>
                     </div>
                     <div class="col s4">
-                    <input placeholder="yyyy-mm-dd&emsp;▼" type="text" class="datepicker" name="tododate">
+                        <input placeholder="yyyy-mm-dd&emsp;▼" type="text" class="datepicker" name="tododate">
                     </div>
                 </div>
-                <input type="hidden" name="todoOf" value="<?php if(isset($_SESSION['tasks'])) {echo $_SESSION['tasks'];}?>">
+                <input type="hidden" name="todoOf"
+                    value="<?php if(isset($_SESSION['tasks'])) {echo $_SESSION['tasks'];}?>">
                 <div class="row">
                     <div class="input-field input-field-todo">
                         <textarea id="todo_task" name="todo_task" class="materialize-textarea todo" required></textarea>
                         <label for="todo_task" id="todo-label">Input to-do here:</label>
                     </div>
                 </div>
-                <button class="waves-effect waves-light btn green" type="submit" class="validate" name="create_todo">Save</button>
+                <button class="waves-effect waves-light btn green" type="submit" class="validate"
+                    name="create_todo">Save</button>
                 <!--HINDI KO MAPAGANA
                    <div id="savetodo" class="modal">
                     <div class="modal-content">
@@ -229,8 +225,8 @@
                                     <button class="waves-effect waves-light btn modal-trigger"
                                         href="#doneBtn">Done</button>
                                     <div id="doneBtn" class="modal">
-                                    <div class="modal-content">
-                                        <span>Are you sure want to click done?</span>
+                                        <div class="modal-content">
+                                            <span>Are you sure want to click done?</span>
                                         </div>
                                         <div class="modal-footer">
                                             <a class="modal-close waves-effect waves-red btn-flat">No</a>
@@ -246,8 +242,8 @@
                                     <button class="waves-effect waves-light btn modal-trigger"
                                         href="#clearBtn">Clear</button>
                                     <div id="clearBtn" class="modal">
-                                    <div class="modal-content">
-                                        <span>Are you sure want to clear this task</span>
+                                        <div class="modal-content">
+                                            <span>Are you sure want to clear this task</span>
                                         </div>
                                         <div class="modal-footer">
                                             <a class="modal-close waves-effect waves-red btn-flat">No</a>
@@ -376,8 +372,7 @@
                                     ?>
                             <input type="hidden" name="todo_id" value="<?php echo $row[0]?>">
                             <input type="hidden" name="todo_status" value="<?php echo $row[3]?>">
-                            <button class="waves-effect waves-light btn modal-trigger"
-                                href="#doneBtn">Done</button>
+                            <button class="waves-effect waves-light btn modal-trigger" href="#doneBtn">Done</button>
                             <div id="doneBtn" class="modal modal-fixed-footer">
                                 <span>Are you sure want to click done?</span>
                                 <div class="modal-footer">
@@ -391,8 +386,7 @@
                                     ?>
                             <input type="hidden" name="todo_id" value="<?php echo $row[0]?>">
                             <input type="hidden" name="todo_status" value="<?php echo $row[3]?>">
-                            <button class="waves-effect waves-light btn modal-trigger"
-                                href="#clearBtn">Clear</button>
+                            <button class="waves-effect waves-light btn modal-trigger" href="#clearBtn">Clear</button>
                             <div id="clearBtn" class="modal modal-fixed-footer">
                                 <span>Are you sure want to clear this task</span>
                                 <div class="modal-footer">
@@ -420,14 +414,15 @@
 
     <!--Import jQuery before materialize.js-->
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/js/materialize.min.js">
+    <script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/js/materialize.min.js">
     </script>
     <script type="text/javascript" src="../datepicker.js"></script>
     <script>
         // SIDEBAR
         // SIDEBAR
-        
-        $(document).ready(function() {
+
+        $(document).ready(function () {
             $('.button-collapse').sideNav({
                 menuWidth: 300, // Default is 300
                 edge: 'left', // Choose the horizontal origin
@@ -439,7 +434,7 @@
 
             $('.modal-trigger').leanModal();
 
-            $(".add-row").click(function() {
+            $(".add-row").click(function () {
                 var quantity = $("#name").val();
                 var unit = $("#email").val();
                 var articles = $('#articles').val();
@@ -450,17 +445,15 @@
             });
         });
 
-        (function($) {
-		$(function() {
+        (function ($) {
+            $(function () {
 
-				$(".dropdown-button").dropdown();
+                $(".dropdown-button").dropdown();
 
-		}); // End Document Ready
-})(jQuery);
-
-
-
+            }); // End Document Ready
+        })(jQuery);
     </script>
 
 </body>
+
 </html>

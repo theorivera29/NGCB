3b8f642eee86baac4f21ef5ac790d1c2569c4109
tuguinaymikka <2164725@slu.edu.ph>
2019-04-09 01:00:07 +0800

    edit project

--- Admin/projects.php
+++ Admin/projects.php
@@ -92,72 +92,115 @@
         <!--ONGOING TAB-->
         <div id="ongoing" class="col s12">
             <div class="row">
-
+                <?php
+                        $sql = "SELECT projects_name, projects_address, projects_sdate, projects_edate FROM projects
+                        WHERE projects_status = 'open';";
+                        $result = mysqli_query($conn, $sql);
+                        while($row = mysqli_fetch_row($result)){
+                    ?>
                 <div class="col s12 m6">
                     <div class="card blue-grey darken-1 center">
                         <div class="card-content white-text">
-                            <span class="card-title"></span>
-                            <p>NGCB SAMPLE PROJECT
-                                <p>start date</p>
-                                <p>end date</p>
-                                <div class="row">
-                                    <form action="server.php" method="POST">
-                                        <input type="hidden" name="projects_name" value="">
+                            <span class="card-title"><?php echo $row[0] ?></span>
+                            <p>
+                                <?php echo $row[1] ?>
+                            </p>
+                            <p>
+                                <span>
+                                    start date
+                                </span>
+                                <?php echo $row[2] ?>
+                            </p>
+                            <p>
+                                <span>
+                                    end date
+                                </span><?php echo $row[3] ?>
+                            </p>
+                            <div class="row">
+                                <form action="server.php" method="POST">
+                                    <input type="hidden" name="projects_name" value="<?php echo $row[0] ?>">
 
+                                    <div class="row">
+                                        <button href="#editModal" class="waves-effect waves-light btn edit-btn modal-trigger">Edit</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <!--
+                                <div class="row">
+                                    <form action="../server.php" method="POST">
+                                        <input type="hidden" name="projects_name" value="<?php echo $row[0] ?>">
                                         <div class="row">
-                                            <a href="#editModal" class="waves-effect waves-light btn edit-btn modal-trigger">Edit</a>
+                                            <button class="waves-effect waves-light btn viewinventory-btn" type="submit"
+                                                name="view_inventory">View Inventory</button>
                                         </div>
                                     </form>
                                 </div>
-
-                                <div class="row">
-                                    <a href="#closeModal" class="waves-effect waves-light btn red modal-trigger">Close
-                                        Project</a>
-                                </div>
-
+-->
+                            <?php 
+                                    if (strtotime($row[2]) > strtotime ($row[3])) {
+                                        ?>
+                            <div class="row">
+                                <a href="#closeModal" class="waves-effect waves-light btn red modal-trigger">Close
+                                    Project</a>
+                            </div>
+                            <?php
+                                    }
+                                ?>
                         </div>
                     </div>
                 </div>
 
-            </div>
-        </div>
-
-    </div>
-
-
-    <!--EDIT MODAL-->
-    <div id="editModal" class="modal modal-fixed-footer">
-        <div class="modal-content">
-            <h4>Edit Project:
-
-            </h4>
-            <form action="server.php" method="POST">
-                <div class="row">
-                    <div class="input-field col s6">
-                        <input placeholder="New project name" id="new_project_name" type="text" class="validate" name="new_project_name">
-                        <label class="active" for="new_project_name">Project Name:</label>
+                <div id="closeModal" class="modal modal-fixed-footer">
+                    <div class="modal-content">
+                        <h4>Close Project?</h4>
+                        <p>Are you sure you want to close this project?</p>
                     </div>
-                    <div class="input-field col s6">
-                        <input placeholder="New address" id="new_address" type="text" class="validate" name="new_address">
-                        <label for="new_address">Address:</label>
-                    </div>
-                    <div class="input-field col s6">
-                        <input placeholder="New start date" id="new_sdate" type="text" class="validate" name="new_sdate">
-                        <label for="new_sdate">Start date:</label>
-                    </div>
-                    <div class="input-field col s6">
-                        <input placeholder="New end date" id="new_edate" type="text" class="validate" name="new_edate">
-                        <label for="new_edate">End date:</label>
+                    <div class="modal-footer">
+                        <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">No</a>
+                        <form action="../server.php" method="POST">
+                            <input type="hidden" name="project_name" value='<?php echo $row[0] ?>'>
+                            <button type="submit" name="close_project" class="modal-action modal-close waves-effect waves-green btn-flat">Yes</button>
+                        </form>
                     </div>
                 </div>
-            </form>
-        </div>
 
-        <div class="modal-footer">
-            <a href="#!" class="modal-close waves-effect waves-red btn-flat">Cancel</a>
-            <input type="hidden" name="project_name" value=''>
-            <button name="edit_project" class="modal-action modal-close waves-effect waves-green btn-flat">Save
-                Changes</button>
+                <!--EDIT MODAL-->
+                <div id="editModal" class="modal modal-fixed-footer">
+                    <div class="modal-content">
+                        <h4>Edit Project:</h4>
+                        <form action="../server.php" method="POST">
+                            <div class="row">
+                                <input type="hidden" name="project_name" value="<?php echo $row[0] ?>">
+                                <div class="input-field col s6">
+                                    <input placeholder="New project name" id="new_project_name" type="text" class="validate" name="new_project_name">
+                                    <label class="active" for="new_project_name">Project Name:</label>
+                                </div>
+                                <div class="input-field col s6">
+                                    <input placeholder="New address" id="new_address" type="text" class="validate" name="new_address">
+                                    <label for="new_address">Address:</label>
+                                </div>
+                                <div class="input-field col s6">
+                                    <input placeholder="New start date" id="new_sdate" type="text" class="validate" name="new_sdate">
+                                    <label for="new_sdate">Start date:</label>
+                                </div>
+                                <div class="input-field col s6">
+                                    <input placeholder="New end date" id="new_edate" type="text" class="validate" name="new_edate">
+                                    <label for="new_edate">End date:</label>
+                                </div>
+                                <div class="modal-footer">
+                                    <a href="#!" class="modal-close waves-effect waves-red btn-flat">Cancel</a>
+                                    <button name="edit_project" class="modal-action modal-close waves-effect waves-green btn-flat">Save
+                                    Changes</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+                <?php 
+                        }
+                    ?>
+            </div>
         </div>
     </div>
 
@@ -170,7 +213,7 @@
         <div class="modal-footer">
             <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">No</a>
             <form action="server.php" method="POST">
-                <input type="hidden" name="project_name" value=''>
+                <input type="hidden" name="project_name">
                 <button type="submit" name="close_project" class="modal-action modal-close waves-effect waves-green btn-flat">Yes</button>
             </form>
         </div>
@@ -250,9 +293,9 @@
                                     $result = mysqli_query($conn, $sql);
                                     while($row = mysqli_fetch_row($result)) {                         
                                 ?>
-                                    <option value="<?php echo $row[0]; ?>">
-                                        <?php echo $row[2]." ".$row[1]; ?>
-                                    </option>
+                                <option value="<?php echo $row[0]; ?>">
+                                    <?php echo $row[2]." ".$row[1]; ?>
+                                </option>
                                 <?php 
                                     }
                                 ?>

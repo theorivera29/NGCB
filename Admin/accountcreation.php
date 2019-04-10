397846f07ec62a30e7b290768362f5d4406f5d62
Jam <jamspica25@gmail.com>
2019-04-11 02:57:27 +0900

    Updated side navigation of admin
    Not yet done

--- Admin/accountcreation.php
+++ Admin/accountcreation.php
@@ -24,57 +24,81 @@
 <body>
     <nav>
         <div class="nav-wrapper">
-            <a href="#" data-activates="mobile-demo" class="button-collapse show-on-large pulse"><i class="material-icons">menu</i></a>
-            <h4 id="NGCB">NEW GOLDEN CITY BUILDERS</h4>
-            <ul class="side-nav" id="mobile-demo">
-                <li class="collection-item avatar">
-                    <span class="title">
-                    </span>
-                    <span class="title">
-                    </span>
+            <a href="#" data-activates="navigation" class="button-collapse show-on-large menu-icon"><i
+                    class="material-icons menuIcon">menu</i></a>
+            <span id="NGCB">NEW GOLDEN CITY BUILDERS AND DEVELOPMENT CORPORATION</span>
+            <?php 
+                            if(isset($_SESSION['username'])) {
+                            $username = $_SESSION['username'];
+                            $sql = "SELECT * FROM accounts WHERE accounts_username = '$username'";
+                            $result = mysqli_query($conn, $sql);
+                            $row = mysqli_fetch_row($result);
+                        ?>
+            <span id="acName">
+                <ul>
+                    <?php echo $row[1]." ".$row[2]; ?>
+                    <li class="down-arrow">
+
+                        <a class="dropdown-button" href="#!" data-activates="dropdown" data-beloworigin="true"><i
+                                class="material-icons dropdown-button">keyboard_arrow_down</i></a>
+                    </li>
+
+                </ul>
+                <ul id="dropdown" class="dropdown-content collection">
+                    <li><a class="waves-effect waves-blue" href="account.php">Account</a></li>
+                    <li><a class="waves-effect waves-blue" href="logout.php">Logout</a></li>
+
+                </ul>
+            </span>
+            <ul class="side-nav" id="navigation">
+                <li class="icon-container">
+                    <img src="../Images/NGCB_logo.png" class="sidenav-logo">
                 </li>
+                <h3 id="account-type">
+                    <?php 
+                        if(strcmp($row[5], "MatEng") == 0 ) {
+                            echo "Materials Engineer";
+                        } else if(strcmp($row[5], "ViewOnly") == 0 ) {
+                            echo "View Only";
+                        } else {
+                            echo "Admin";
+                        }
+                        }
+                    ?>
+                </h3>
+
                 <li>
-                    <div class="divider"></div>
+                    <i class="material-icons left">dashboard</i><a class="waves-effect waves-blue"
+                        href="admindashboard.php">Dashboard</a>
                 </li>
-                <li><a href="admindashboard.php">Dashboard</a></li>
-                <li>
-                    <div class="divider"></div>
-                </li>
+
 
                 <ul class="collapsible">
                     <li>
-                        <a class="collapsible-header waves-effect waves-blue">Accounts<i class="material-icons right">keyboard_arrow_down</i></a>
+                        <i class="material-icons left">supervisor_account</i><a
+                            class="collapsible-header waves-effect waves-blue">Accounts<i
+                                class="material-icons right">keyboard_arrow_down</i></a>
                         <div class="collapsible-body">
                             <ul>
-                                <li>
-                                    <a class="waves-effect waves-blue" href="accountcreation.php">Create Account</a>
+                                <li><a class="waves-effect waves-blue" href="accountcreation.php">Create Account</a>
                                 </li>
-                                <li>
-                                    <a class="waves-effect waves-blue" href="listofaccounts.php">List of Accounts</a>
+                                <li><a class="waves-effect waves-blue" href="listofaccounts.php">List of Accounts</a>
                                 </li>
                             </ul>
                         </div>
                     </li>
                 </ul>
                 <li>
-                    <div class="divider"></div>
-                </li>
-                <li><a href="projects.php">Projects</a></li>
-
-                <li>
-                <li>
-                    <div class="divider"></div>
-                </li>
-
-
-
-                <li><a href="passwordrequest.php">Password Request</a></li>
-                <li>
-                <li>
-                    <div class="divider"></div>
+                    <i class="material-icons left">insert_drive_file</i><a class="waves-effect waves-blue"
+                        href="projects.php">Projects</a>
                 </li>
                 <li>
-                    <a href="../logout.php">Logout</a>
+                    <i class="material-icons left">folder</i><a class="waves-effect waves-blue"
+                        href="report.php">Logs</a>
+                </li>
+                <li>
+                    <i class="material-icons left">receipt</i><a class="waves-effect waves-blue"
+                        href="report.php">Report</a>
                 </li>
             </ul>
         </div>
@@ -85,53 +109,60 @@
             <h2 class="header header-two">Create an Account</h2>
             <div class="row">
                 <div class="input-field col s12 m10 offset-m1">
-                    <input id="firstname" name="firstname" type="text" class="validate" pattern="[A-Za-z]*" title="Input only letters" required>
+                    <input id="firstname" name="firstname" type="text" class="validate" pattern="[A-Za-z]*"
+                        title="Input only letters" required>
                     <label for="firstname">First Name</label>
                 </div>
 
                 <div class="input-field col s12 m10 offset-m1">
-                    <input id="lastname" name="lastname" type="text" class="validate" pattern="[A-Za-z]*" title="Input only letters" required>
+                    <input id="lastname" name="lastname" type="text" class="validate" pattern="[A-Za-z]*"
+                        title="Input only letters" required>
                     <label for="lastname">Last Name</label>
                 </div>
 
                 <div class="input-field col s12 m10 offset-m1">
-                    <input id="username" name="username" type="text" class="validate" pattern="[A-Za-z0-9]*" title="Input only letters" required>
+                    <input id="username" name="username" type="text" class="validate" pattern="[A-Za-z0-9]*"
+                        title="Input only letters" required>
                     <label for="username">Username</label>
                 </div>
 
                 <div class="input-field col s12 m10 offset-m1">
-                    <input id="email" name="email" type="text" class="validate" pattern="[A-Za-z0-9]*@[A-Za-z]*\.[A-Za-z]*" title="Follow the format. Example: email@email.com" required>
+                    <input id="email" name="email" type="text" class="validate"
+                        pattern="[A-Za-z0-9]*@[A-Za-z]*\.[A-Za-z]*" title="Follow the format. Example: email@email.com"
+                        required>
                     <label for="email">Email</label>
                 </div>
 
                 <div class="input-field col s12 m10 offset-m1">
-                    <input id="password" name="password" type="password" class="validate" minlength="6" maxlength="12" title="Alphanumeric only" required>
+                    <input id="password" name="password" type="password" class="validate" minlength="6" maxlength="12"
+                        title="Alphanumeric only" required>
                     <label for="password">Password</label>
                 </div>
 
                 <div class="col s12 m10 offset-m1">
                     <span>Account Type</span>
                     <div class="row">
-                       <form>   
-                        <p>
-                            <label>
-                                <input class="with-gap" value="MatEng" type="radio"/>
-                                <span>Materials Engineer</span>
-                            </label>
-                        </p>
-                        <p>
-                            <label>
-                                <input class="with-gap" value="ViewOnly" type="radio"/>
-                                <span>View-Only</span>
-                            </label>
-                        </p>
-                       </form>
+                        <form>
+                            <p>
+                                <label>
+                                    <input class="with-gap" value="MatEng" type="radio" />
+                                    <span>Materials Engineer</span>
+                                </label>
+                            </p>
+                            <p>
+                                <label>
+                                    <input class="with-gap" value="ViewOnly" type="radio" />
+                                    <span>View-Only</span>
+                                </label>
+                            </p>
+                        </form>
                     </div>
 
                 </div>
 
                 <div class="row center">
-                    <button class="btn waves-effect waves-light create-account-btn" type="submit" name="create_account">Create
+                    <button class="btn waves-effect waves-light create-account-btn" type="submit"
+                        name="create_account">Create
                         An Account</button>
                 </div>
             </div>
@@ -144,7 +175,7 @@
     </script>
     <script>
         // SIDEBAR
-        $(document).ready(function() {
+        $(document).ready(function () {
             $('.button-collapse').sideNav({
                 menuWidth: 300, // Default is 300
                 edge: 'left', // Choose the horizontal origin
@@ -154,7 +185,6 @@
             // START OPEN
             $('.button-collapse').sideNav('show');
         });
-
     </script>
 
 </body>

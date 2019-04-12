02595a336388b86a715ba1422ca44891ed77a676
jamspicaa <Jamspica25@gmail.com>
2019-04-13 03:46:06 +0800

    Update openhauling.php

--- Materials Engineer/openhauling.php
+++ Materials Engineer/openhauling.php
@@ -119,137 +119,139 @@
                             $result = mysqli_query($conn, $sql);
                             while($row = mysqli_fetch_row($result)){
                         ?>
-    <div class="row">
-        <div class="col s12 m10 offset-m1">
-            <div class="card light-blue lighten-5">
-                <div class="card-content sample">
-
-                    <h4>Hauling Form</h4>
-                    <form class="sample">
-                        <div class="row">
-                            <div class="col s8">
-                                <label for="date">Date:</label>
-                                <input id="date" type="text" disabled value="<?php echo $row[1]?>">
-                            </div>
-                            <div class="input-field col s2">
-                                <input disabled value="<?php echo $row[0]?>" id="formnumber" type="text">
-                                <label for="formnumber">Form No.:</label>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col s6">
-                                <div>
-                                    <div class="input-field col s12 left-align ">
-                                        <input disabled value="<?php echo $row[2]?>" id="delivername" type="text"
-                                            class="validate">
-                                        <label for="delivername">Deliver To:</label>
+    <div class="row fill-hauling-form-container">
+        <div class="col haulingform-container">
+            <div class="card hauling-form">
+                <div class="fillout-content">
+                    <div class="row">
+                        <div class="col">
+                            <h4>Hauling Form</h4>
+                            <form class="sample">
+                                <div class="row">
+                                    <div class="col s8">
+                                        <label for="date">Date:</label>
+                                        <input id="date" type="text" disabled value="<?php echo $row[1]?>">
                                     </div>
-                                    <div class="input-field col s12 left-align ">
-                                        <input disabled value="<?php echo $row[3]?>" id="hauledfrom" type="text"
-                                            class="validate">
-                                        <label for="hauledfrom">Hauled From:</label>
+                                    <div class="input-field col s2">
+                                        <input disabled value="<?php echo $row[0]?>" id="formnumber" type="text">
+                                        <label for="formnumber">Form No.:</label>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="col s12">
-                            <table class="striped centered">
-                                <thead>
-                                    <tr>
-                                        <th>Quantity</th>
-                                        <th>Unit</th>
-                                        <th>Articles</th>
-                                    </tr>
-                                </thead>
-
-                                <tbody>
-                                    <tr>
-                                        <td><?php echo $row[4]?></td>
-                                        <td><?php echo $row[5]?></td>
-                                        <td><?php echo $row[6]?></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-
-                        <div class="row">
-                            <div class="col s6">
-                                <div class="input-field col s10 left-align ">
-                                    <input disabled value="<?php echo $row[7]?>" id="hauledby" type="text"
-                                        class="validate">
-                                    <label for="hauledby">Hauled By:</label>
+                                <div class="row">
+                                    <div class="col s6">
+                                        <div>
+                                            <div class="input-field col s12 left-align ">
+                                                <input disabled value="<?php echo $row[2]?>" id="delivername"
+                                                    type="text" class="validate">
+                                                <label for="delivername">Deliver To:</label>
+                                            </div>
+                                            <div class="input-field col s12 left-align ">
+                                                <input disabled value="<?php echo $row[3]?>" id="hauledfrom" type="text"
+                                                    class="validate">
+                                                <label for="hauledfrom">Hauled From:</label>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="input-field col s10 left-align ">
-                                    <input disabled value="<?php echo $row[8]?>" id="warehouseman" type="text"
-                                        class="validate">
-                                    <label for="warehouseman">Warehouseman:</label>
-                                </div>
-                                <div class="input-field col s10 left-align ">
-                                    <input disabled value="<?php echo $row[9]?>" id="approvedby" type="text"
-                                        class="validate">
-                                    <label for="approvedby">Approved By:</label>
-                                </div>
-                            </div>
-                            <div class="col s6">
-                                <table class="striped centered">
-                                    <thead>
+                                <div class="col s12">
+                                    <table class="striped centered">
+                                        <thead>
+                                            <tr>
+                                                <th>Quantity</th>
+                                                <th>Unit</th>
+                                                <th>Articles</th>
+                                            </tr>
+                                        </thead>
 
-                                        <tr>
-                                            <th> </th>
-                                            <th>Truck details</th>
-                                            <th> </th>
-                                        </tr>
-                                    </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td><?php echo $row[4]?></td>
+                                                <td><?php echo $row[5]?></td>
+                                                <td><?php echo $row[6]?></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
 
-                                    <tbody>
-                                        <tr>
-                                            <td>Type:</td>
-                                            <td><?php echo $row[10]?></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Plate No.:</td>
-                                            <td><?php echo $row[11]?></td>
-                                        </tr>
-                                        <tr>
-                                            <td>P.O/R.S No.:</td>
-                                            <td><?php echo $row[12]?></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Hauler DR No.:</td>
-                                            <td><?php echo $row[13]?></td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <?php
+                                <div class="row">
+                                    <div class="col s6">
+                                        <div class="input-field col s10 left-align ">
+                                            <input disabled value="<?php echo $row[7]?>" id="hauledby" type="text"
+                                                class="validate">
+                                            <label for="hauledby">Hauled By:</label>
+                                        </div>
+                                        <div class="input-field col s10 left-align ">
+                                            <input disabled value="<?php echo $row[8]?>" id="warehouseman" type="text"
+                                                class="validate">
+                                            <label for="warehouseman">Warehouseman:</label>
+                                        </div>
+                                        <div class="input-field col s10 left-align ">
+                                            <input disabled value="<?php echo $row[9]?>" id="approvedby" type="text"
+                                                class="validate">
+                                            <label for="approvedby">Approved By:</label>
+                                        </div>
+                                    </div>
+                                    <div class="col s6">
+                                        <table class="striped centered">
+                                            <thead>
+
+                                                <tr>
+                                                    <th> </th>
+                                                    <th>Truck details</th>
+                                                    <th> </th>
+                                                </tr>
+                                            </thead>
+
+                                            <tbody>
+                                                <tr>
+                                                    <td>Type:</td>
+                                                    <td><?php echo $row[10]?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Plate No.:</td>
+                                                    <td><?php echo $row[11]?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>P.O/R.S No.:</td>
+                                                    <td><?php echo $row[12]?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Hauler DR No.:</td>
+                                                    <td><?php echo $row[13]?></td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <?php
         }
     ?>
+                                </div>
+                            </form>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 
 
-    <!--Import jQuery before materialize.js-->
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/js/materialize.js">
-    </script>
-    <script>
-        // SIDEBAR
-        $(document).ready(function () {
-            $('.button-collapse').sideNav({
-                menuWidth: 300, // Default is 300
-                edge: 'left', // Choose the horizontal origin
-                closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-                draggable: true // Choose whether you can drag to open on touch screens
-            });
-            // START OPEN
-            $('.button-collapse').sideNav('show');
+            <!--Import jQuery before materialize.js-->
+            <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+            <script type="text/javascript"
+                src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/js/materialize.js">
+            </script>
+            <script>
+                // SIDEBAR
+                $(document).ready(function () {
+                    $('.button-collapse').sideNav({
+                        menuWidth: 300, // Default is 300
+                        edge: 'left', // Choose the horizontal origin
+                        closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+                        draggable: true // Choose whether you can drag to open on touch screens
+                    });
+                    // START OPEN
+                    $('.button-collapse').sideNav('show');
 
-        });
-    </script>
+                });
+            </script>
 
 </body>
 

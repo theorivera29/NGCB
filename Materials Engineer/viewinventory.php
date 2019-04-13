9b64aae34443f1e080dbd18f48a115b681c2634d
tuguinaymikka <2164725@slu.edu.ph>
2019-04-13 21:42:31 +0800

    edit ng threshold

--- Materials Engineer/viewinventory.php
+++ Materials Engineer/viewinventory.php
@@ -272,8 +272,7 @@
                                                 </select>
                                             </div>
                                             <div class="input-field col s7">
-                                                <input id="minquantity" name="minquantity" type="text" class="validate"
-                                                    required>
+                                                <input id="minquantity" name="minquantity" type="text" class="validate view-inventory" pattern="[0-9]*" title="Input numbers only" required>
                                                 <label for="minquantity">Item threshold:</label>
                                             </div>
                                         </div>

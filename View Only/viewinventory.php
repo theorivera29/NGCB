ff18e2ada98184c080200d1c6ffd393a03a14733
benzservidad <benzservidad1313@gmail.com>
2019-04-16 13:48:15 +0800

    Made sorting (ViewOnly)

--- View Only/viewinventory.php
+++ View Only/viewinventory.php
@@ -171,19 +171,19 @@
             }
         ?>
          <div class="view-inventory-container">
-            <table class="centered view-inventory">
+            <table id = "sort" class="centered view-inventory">
                 <thead class="view-inventory-head">
                         <tr>
-                            <th>Particulars</th>
-                            <th>Previous Material Stock</th>
-                            <th>Delivered Material as of
+                            <th onclick="sortTable(0)">Particulars</th>
+                            <th onclick="sortTable(1)">Previous Material Stock</th>
+                            <th onclick="sortTable(2)">Delivered Material as of
                                 <?php echo date("F Y"); ?>
                             </th>
-                            <th>Material Pulled out as of
+                            <th onclick="sortTable(3)">Material Pulled out as of
                                 <?php echo date("F Y"); ?>
                             </th>
-                            <th>Accumulate of Materials Delivered</th>
-                            <th>Material on Site as of
+                            <th onclick="sortTable(4)">Accumulate of Materials Delivered</th>
+                            <th onclick="sortTable(5)">Material on Site as of
                                 <?php echo date("F Y"); ?>
                             </th>
                         </tr>
@@ -206,10 +206,10 @@
                             foreach($categories as $data) {
                             $categ = $data['categories_name'];
                         ?>
-                        <tr>
+                        <!-- <tr>
                             <td colspan="10" class="td-category"> <b>
                                     <?php echo $categ; ?></b></td>
-                        </tr>
+                        </tr> -->
                         <?php 
                             $sql = "SELECT
                             materials.mat_name, 
@@ -379,6 +379,60 @@
                 $("table tbody").append(markup);
             });
         });
+  function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("sort");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
     </script>
 
 </body>

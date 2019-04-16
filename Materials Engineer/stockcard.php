51ebf5877daa488f3be19e2d95c3f3db8a0f54e2
2166538 <2166538>
2019-04-16 14:52:11 +0800

    added total in stockcard

--- Materials Engineer/stockcard.php
+++ Materials Engineer/stockcard.php
@@ -145,7 +145,18 @@ $mat_name = $_GET['mat_name'];
                             <td>
                                 <?php echo $row_devIn[3] ?>
                             </td>
-
+                            </tr>
+                             <?php 
+                        }
+                        ?>
+                        <?php 
+                        $sql_total = "SELECT SUM(delivered_quantity) FROM deliveredin as total_deliveredin  WHERE delivered_matname = '$mat_id';";
+                        $result_total = mysqli_query($conn, $sql_total);
+                        while($row_total = mysqli_fetch_row($result_total)){
+                        ?>
+                        <tr>
+                        <td>TOTAL:</td>
+                        <td><?php echo $row_total[0]?></td>
                         </tr>
                         <?php 
                         }
@@ -228,11 +239,26 @@ $mat_name = $_GET['mat_name'];
                                 <?php echo $row_useIn[4] ?>
                             </td>
                         </tr>
-                        <?php 
-                        }
+                          <?php 
                         }
                         ?>
-                    </tbody>
+                        <?php 
+                        $sql_total = "SELECT SUM(usage_quantity) FROM usagein as total_usagein  WHERE usage_matname = '$mat_id';";
+                        $result_total = mysqli_query($conn, $sql_total);
+                        while($row_total = mysqli_fetch_row($result_total)){
+                        ?>
+                        <tr>
+                            <td>
+                                TOTAL:
+                            </td>
+                            <td>
+                                <?php echo $row_total[0] ?>
+                            </td>
+                        </tr>
+                    <?php
+                        }
+                        }
+                            ?>
                 </table>
                 <div class="stockcard-btn">
                     <button class="waves-effect waves-light btn save-stockcard-btn" type="submit" class="validate"

187d3b1ce8c7e3e591eee523a3e4424dd093b15b
benzservidad <benzservidad1313@gmail.com>
2019-04-27 02:11:51 +0800

    put sorting in itemcategories

--- View Only/itemcategories.php
+++ View Only/itemcategories.php
@@ -98,24 +98,24 @@
     ?>
         <div class="row">
             <div class="col s12">
-                <table class="centered ">
+                <table id="sort" class="centered ">
                     <thead class="item-categories-head">
                         <tr>
-                            <th>Particulars</th>
-                            <th>Previous Material Stock</th>
-                            <th>Unit</th>
-                            <th>Delivered Material as of 
+                            <th onClick="javascript:SortTable(0,'T');">Particulars</th>
+                            <th onClick="javascript:SortTable(1,'N');">Previous Material Stock</th>
+                            <th onClick="javascript:SortTable(2,'T');">Unit</th>
+                            <th onClick="javascript:SortTable(3,'N');">Delivered Material as of 
                             <?php echo date("F Y"); ?>
                             </th>
-                            <th>Material Pulled out as of
+                            <th onClick="javascript:SortTable(4,'N');">Material Pulled out as of
                             <?php echo date("F Y"); ?>
                             </th>
-                            <th>Unit</th>
-                            <th>Accumulate of Materials Delivered</th>
-                            <th>Material on Site as of 
+                            <th onClick="javascript:SortTable(5,'T');">Unit</th>
+                            <th onClick="javascript:SortTable(6,'N');">Accumulate of Materials Delivered</th>
+                            <th onClick="javascript:SortTable(7,'N');">Material on Site as of 
                             <?php echo date("F Y"); ?>
                             </th>
-                            <th>Unit</th>
+                            <th onClick="javascript:SortTable(8,'T');">Unit</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -127,9 +127,11 @@
                             materials.delivered_material, 
                             materials.pulled_out, 
                             materials.accumulated_materials,
-                            materials.currentQuantity
+                            materials.currentQuantity,
+                            unit.unit_name
                             FROM materials 
                             INNER JOIN categories ON materials.mat_categ = categories.categories_id
+                            INNER JOIN unit ON materials.mat_unit = unit.unit_id
                             WHERE categories.categories_id = '$categories_id';";
                             $result = mysqli_query($conn, $sql);
                             while($row = mysqli_fetch_row($result)){
@@ -141,21 +143,27 @@
                             <td>
                                 <?php echo $row[1] ?>
                             </td>
-                            <td>Unit</td>
+                            <td>
+                                <?php echo $row[6] ?>
+                            </td>
                             <td>
                                 <?php echo $row[2] ?>
                             </td>
                             <td>
                                 <?php echo $row[3] ?>
                             </td>
-                            <td>Unit</td>
+                            <td>
+                                <?php echo $row[6] ?>
+                            </td>
                             <td>
                                 <?php echo $row[4] ?>
                             </td>
                             <td>
                                 <?php echo $row[5] ?>
                             </td>
-                            <td>Unit</td>
+                            <td>
+                                <?php echo $row[6] ?>
+                            </td>
                         </tr>
 
                         <?php    
@@ -184,6 +192,61 @@
             $('.collapsible').collapsible();
             $('.modal-trigger').leanModal();
         });
+
+        var TableIDvalue = "sort";
+        var TableLastSortedColumn = -1;
+
+        function SortTable() {
+            var sortColumn = parseInt(arguments[0]);
+            var type = arguments.length > 1 ? arguments[1] : 'T';
+            var dateformat = arguments.length > 2 ? arguments[2] : '';
+            var table = document.getElementById(TableIDvalue);
+            var tbody = table.getElementsByTagName("tbody")[0];
+            var rows = tbody.getElementsByTagName("tr");
+            var arrayOfRows = new Array();
+            type = type.toUpperCase();
+            dateformat = dateformat.toLowerCase();
+            for (var i = 0, len = rows.length; i < len; i++) {
+                arrayOfRows[i] = new Object;
+                arrayOfRows[i].oldIndex = i;
+                var celltext = rows[i].getElementsByTagName("td")[sortColumn].innerHTML.replace(/<[^>]*>/g, "");
+                if (type == 'D') {
+                    arrayOfRows[i].value = GetDateSortingKey(dateformat, celltext);
+                } else {
+                    var re = type == "N" ? /[^\.\-\+\d]/g : /[^a-zA-Z0-9]/g;
+                    arrayOfRows[i].value = celltext.replace(re, "").substr(0, 25).toLowerCase();
+                }
+            }
+            if (sortColumn == TableLastSortedColumn) {
+                arrayOfRows.reverse();
+            } else {
+                TableLastSortedColumn = sortColumn;
+                switch (type) {
+                    case "N":
+                        arrayOfRows.sort(CompareRowOfNumbers);
+                        break;
+                    default:
+                        arrayOfRows.sort(CompareRowOfText);
+                }
+            }
+            var newTableBody = document.createElement("tbody");
+            for (var i = 0, len = arrayOfRows.length; i < len; i++) {
+                newTableBody.appendChild(rows[arrayOfRows[i].oldIndex].cloneNode(true));
+            }
+            table.replaceChild(newTableBody, tbody);
+        } // function SortTable()
+
+        function CompareRowOfText(a, b) {
+            var aval = a.value;
+            var bval = b.value;
+            return (aval == bval ? 0 : (aval > bval ? 1 : -1));
+        } // function CompareRowOfText()
+
+        function CompareRowOfNumbers(a, b) {
+            var aval = /\d/.test(a.value) ? parseFloat(a.value) : 0;
+            var bval = /\d/.test(b.value) ? parseFloat(b.value) : 0;
+            return (aval == bval ? 0 : (aval > bval ? 1 : -1));
+        } // function CompareRowOfNumbers()
     </script>
 
 </body>

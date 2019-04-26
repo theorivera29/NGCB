79b49154f5a69c134495345a0e569fdeb4328b4f
benzservidad <benzservidad1313@gmail.com>
2019-04-27 03:24:27 +0800

    Reverted old sorting function in low quantity

--- Materials Engineer/dashboard.php
+++ Materials Engineer/dashboard.php
@@ -255,17 +255,17 @@
             <table id="sort" class="striped responsive-table materials-left centered">
                 <thead class="view-materials-head">
                     <tr class="material-headers">
-                        <th onClick="javascript:SortTable(0,'T');">Material Name<i
+                        <th onclick="sortTable(0)">Material Name<i
                         class="material-icons tiny sort-icon">code</i></th>
-                        <th onClick="javascript:SortTable(1,'T');">Category<i
+                        <th onclick="sortTable(1)">Category<i
                         class="material-icons tiny sort-icon">code</i></th>
-                        <th onClick="javascript:SortTable(2,'N');">Quantity Remaining<i
+                        <th onclick="sortTable(2)">Quantity Remaining<i
                         class="material-icons tiny sort-icon">code</i></th>
-                        <th onClick="javascript:SortTable(3,'T');">Unit<i
+                        <th onclick="sortTable(3)">Unit<i
                         class="material-icons tiny sort-icon">code</i></th>
-                        <th onClick="javascript:SortTable(4,'N');">Threshold<i
+                        <th onclick="sortTable(4)">Threshold<i
                         class="material-icons tiny sort-icon">code</i></th>
-                        <th onClick="javascript:SortTable(5,'T');">Project<i
+                        <th onclick="sortTable(5)">Project<i
                         class="material-icons tiny sort-icon">code</i></th>
                     </tr>
                 </thead>
@@ -352,60 +352,42 @@
             $('#characters').text(length);
         });
 
-        var TableIDvalue = "sort";
-        var TableLastSortedColumn = -1;
-
-        function SortTable() {
-            var sortColumn = parseInt(arguments[0]);
-            var type = arguments.length > 1 ? arguments[1] : 'T';
-            var dateformat = arguments.length > 2 ? arguments[2] : '';
-            var table = document.getElementById(TableIDvalue);
-            var tbody = table.getElementsByTagName("tbody")[0];
-            var rows = tbody.getElementsByTagName("tr");
-            var arrayOfRows = new Array();
-            type = type.toUpperCase();
-            dateformat = dateformat.toLowerCase();
-            for (var i = 0, len = rows.length; i < len; i++) {
-                arrayOfRows[i] = new Object;
-                arrayOfRows[i].oldIndex = i;
-                var celltext = rows[i].getElementsByTagName("td")[sortColumn].innerHTML.replace(/<[^>]*>/g, "");
-                if (type == 'D') {
-                    arrayOfRows[i].value = GetDateSortingKey(dateformat, celltext);
+        function sortTable(n) {
+            var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+            table = document.getElementById("sort");
+            switching = true;
+            dir = "asc";
+            while (switching) {
+                switching = false;
+                rows = table.rows;
+                for (i = 1; i < (rows.length - 1); i++) {
+                    shouldSwitch = false;
+                    x = rows[i].getElementsByTagName("TD")[n];
+                    y = rows[i + 1].getElementsByTagName("TD")[n];
+                    if (dir == "asc") {
+                        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                            shouldSwitch = true;
+                            break;
+                        }
+                    } else if (dir == "desc") {
+                        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                            shouldSwitch = true;
+                            break;
+                        }
+                    }
+                }
+                if (shouldSwitch) {
+                    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                    switching = true;
+                    switchcount++;
                 } else {
-                    var re = type == "N" ? /[^\.\-\+\d]/g : /[^a-zA-Z0-9]/g;
-                    arrayOfRows[i].value = celltext.replace(re, "").substr(0, 25).toLowerCase();
+                    if (switchcount == 0 && dir == "asc") {
+                        dir = "desc";
+                        switching = true;
+                    }
                 }
             }
-            if (sortColumn == TableLastSortedColumn) {
-                arrayOfRows.reverse();
-            } else {
-                TableLastSortedColumn = sortColumn;
-                switch (type) {
-                    case "N":
-                        arrayOfRows.sort(CompareRowOfNumbers);
-                        break;
-                    default:
-                        arrayOfRows.sort(CompareRowOfText);
-                }
-            }
-            var newTableBody = document.createElement("tbody");
-            for (var i = 0, len = arrayOfRows.length; i < len; i++) {
-                newTableBody.appendChild(rows[arrayOfRows[i].oldIndex].cloneNode(true));
-            }
-            table.replaceChild(newTableBody, tbody);
-        } // function SortTable()
-
-        function CompareRowOfText(a, b) {
-            var aval = a.value;
-            var bval = b.value;
-            return (aval == bval ? 0 : (aval > bval ? 1 : -1));
-        } // function CompareRowOfText()
-
-        function CompareRowOfNumbers(a, b) {
-            var aval = /\d/.test(a.value) ? parseFloat(a.value) : 0;
-            var bval = /\d/.test(b.value) ? parseFloat(b.value) : 0;
-            return (aval == bval ? 0 : (aval > bval ? 1 : -1));
-        } // function CompareRowOfNumbers()
+        }
 
         function myFunction() {
             var input, filter, table, tr, td, i, txtValue;

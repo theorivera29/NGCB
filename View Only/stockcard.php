0b1340192a3396d4b338c9cee11e4a25f7bf447b
benzservidad <benzservidad1313@gmail.com>
2019-04-27 03:24:42 +0800

    Removed excess code

--- View Only/stockcard.php
+++ View Only/stockcard.php
@@ -281,14 +281,6 @@
             }
         }
     </script>
-</body>
-
-</html>
-
-    <!--Import jQuery before materialize.js-->
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/js/materialize.js">
-    </script>
     <script>
         // SIDEBAR
         $(document).ready(function () {
